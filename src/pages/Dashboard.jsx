14fab2df826de3728f227d9a18d6d866f57c3554
Navid <nh222yr@student.lnu.se>
2026-04-22 13:06:08 +0200

    uppdated h1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
-import SalesBygeGenre from '../components/charts/SalesByGenre'
-import SalesOvertime from '../components/charts/SalesOverTime'
+import SalesByGenre from '../components/charts/SalesByGenre'
+import SalesOverTime from '../components/charts/SalesOverTime'
 import SalesByPlatform from '../components/charts/SalesByPlatform'
 import TopPublishers from '../components/charts/TopPublishers'
 
 export default function Dashboard() {
   return (
     <div>
-      <h1 className="text-2xl font bold mb-2"> Video Games Sales Dashboard</h1>
-      <p className="text-gray-400 mb-8">16598 spel 1980-2010 NA,EU, JP och global </p>
+      <h1 className="text-2xl font-bold mb-2">Video Game Sales Dashboard</h1>
+      <p className="text-gray-400 mb-8">16 598 spel 1980-2020 NA, EU, JP och globalt</p>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <SalesOverTime />
         <SalesByGenre />
